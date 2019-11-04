742bcb1334e13f11f4804f3a9e86d5b83594111f
Ramon de Paula Marques <ramonpm@gmail.com>
2019-11-04 17:04:21 -0300

    codeship

--- deploy_review_app.sh
+++ deploy_review_app.sh
@@ -2,4 +2,4 @@
 set -x
 set -e
 
-echo "Deploying $CI_ENVIRONMENT_SLUG to git-shoutout-${CI_ENVIRONMENT_SLUG}"
+echo "Deploying $CI_ENVIRONMENT_SLUG to git-shoutout-${CI_ENVIRONMENT_SLUG}.us-west-2.elasticbeanstalk.com"
