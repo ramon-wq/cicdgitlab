c02947b1f63293b70e599a66d851a33cade3971e
Ramon de Paula Marques <ramonpm@gmail.com>
2019-11-04 18:38:16 -0300

    ramon/travis

--- deploy_review_app.sh
+++ deploy_review_app.sh
@@ -2,4 +2,5 @@
 set -x
 set -e
 
+export CI_ENVIRONMENT_SLUG=`echo $TRAVIS_PULL_REQUEST_BRANCH | sed 's/[^a-z0-9]/-/g'`
 echo "Deploying $CI_ENVIRONMENT_SLUG to git-shoutout-${CI_ENVIRONMENT_SLUG}.us-west-2.elasticbeanstalk.com"
